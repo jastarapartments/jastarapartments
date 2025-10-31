fc69820a929a02ed2b9f007ca97dc11070bd27fa
jastarapartments <jastarapartments@gmail.com>
2025-11-01 02:59:38 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+// js/supabase.js
+const SUPABASE_URL = "https://vfignoxzqjjmghzsyyqr.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmaWdub3h6cWpqbWdoenN5eXFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NDU4MTIsImV4cCI6MjA3NzQyMTgxMn0.1sRa8C4vnwYs3ll9CwExBJ6aoLwG924CUpKRWs7B_ww";
 
-export const supabase = createClient(
-  "https://YOUR_PROJECT_ID.supabase.co",
-  "YOUR_PUBLIC_ANON_KEY"
-);
+const { createClient } = window.supabase;
+window.supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
